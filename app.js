e29f6c6b6fbde0d64074dd6e89ff65b657099459
eyupGuc <meydanguresi@gmail.com>
2022-09-29 12:19:49 +0300

    poke type

--- app.js
+++ app.js
@@ -4,8 +4,30 @@ const pokeFetch = async () => {
 
   const data = await res.json();
   console.log(data);
-  console.log(data.name);
-  console.log(data.weight);
-  console.log(first);
+  console.log(data.types[0].type.name);
+  console.log(data.id);
+  updateDOM(data);
+
+  //   <img
+  //     src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+  //     alt="Bulbasaur"
+  //   ></img>;
 };
+
+const updateDOM = (poke) => {
+  const rowDiv = document.querySelector(".row");
+  //   console.log(rowDiv);
+
+  rowDiv.innerHTML += `<div class="card text-center" style="width: 18rem;">
+  <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" class="card-img-top" alt="...">
+  <div class="card-body ">
+    <h2 class="card-text ">${poke.name.toUpperCase()}</h2>
+    <p> Weight: ${poke.weight} Kg</p>
+    <p> Height: ${(poke.height / 3.2808399).toFixed(2)} M</p>
+    <p> Type: ${poke.types[0].type.name} </p>
+
+  </div>
+</div>`;
+};
+
 pokeFetch();
